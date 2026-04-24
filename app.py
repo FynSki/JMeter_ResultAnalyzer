c6c44a04f9dee00c35e7df69714994cb5b3d274d
FynSki <adam3768@gmail.com>
2026-04-24 23:55:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -751,3 +751,8 @@ def jmx_compare():
 
 if __name__ == '__main__':
     app.run(debug=True, port=5050)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5050))
+    app.run(host='0.0.0.0', port=port)
